f042513620fb201f278a2feec9b7924b9dd2d087
EmiriNakamori <emiri060414@gmail.com>
2024-12-24 20:28:00 +0900

    Initial Commit

---
+++ TCAApp/TCAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAAppApp.swift
+//  TCAApp
+//
+//  Created by えみり on 2024/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
